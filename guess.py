20024d2a6dac68fbd09431c618c29ecbafd07c48
HSY <tkaging@gmail.com>
2019-01-11 13:33:44 +0800

    'final'

--- guess.py
+++ guess.py
@@ -1,10 +1,13 @@
 import random
-
-r = random.randint(1, 100)
+s = input('請決定隨機數字範圍開始值:') 
+e = input('請決定隨機數字範圍結束值:')
+s = int(s)
+e = int(e)
+r = random.randint(s, e)
 count = 0
 while True:
 	count += 1 # count = count + 1
-	n = input('猜一個數子(1~100):')
+	n = input('猜一個數子:')
 	n = int(n)
 	if n == r:
 		print('終於猜對了!')
